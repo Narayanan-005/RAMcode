5c473b0f2ab76153a5054b3b39bc539c66497a7f
Narayanan-005 <142734364+Narayanan-005@users.noreply.github.com>
2024-04-10 00:09:57 +0530

    Add files via upload

---
+++ five_program/ArrayElements.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class ArrayElements {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        //An array elements are always stored in sequential memory locations.
+        //because it has continuous block of memory
+    }
+}
